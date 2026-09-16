f43486b9c59bfd62627ed3cf1bd4a9ad6728e91b
thoughtleaderonthoughts <Nikhil143@gmail.com>
2026-09-16 13:15:37 -0700

    Connect Bright Reads to the Supabase project

--- app-config.js
+++ app-config.js
@@ -1,5 +1,5 @@
 // Public client configuration only. Never put a secret/service-role key here.
 window.BRIGHT_READS_CONFIG = {
-  supabaseUrl: '',
-  supabasePublishableKey: ''
+  supabaseUrl: 'https://rqhyulcwbajiscrnjmqq.supabase.co',
+  supabasePublishableKey: 'sb_publishable_F3pm62VsSYkG9Bp3MjYBnQ_CpOLS36N'
 };
